fcecf7aa340cda5a320f2bedc52fa5f1054f0d39
Ledmington <filippo.barbari@gmail.com>
2024-10-02 22:40:41 +0200

    Fixed task2 code

--- main.cpp
+++ main.cpp
@@ -10,6 +10,26 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+void save_image(const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic,
+									Eigen::RowMajor>& m,
+				const std::string& file_name) {
+	// convert original image in one made of bytes instead of doubles
+	Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic,
+				  Eigen::RowMajor>
+		tmp(m.rows(), m.cols());
+	tmp = m.unaryExpr([](double val) -> unsigned char {
+		return static_cast<unsigned char>(val);
+	});
+	// Save the image
+	if (stbi_write_png(file_name.c_str(), tmp.cols(), tmp.rows(), 1, tmp.data(),
+					   tmp.cols()) == 0) {
+		std::cerr << " \u001b[31mERROR\u001b[0m: Could not save image to "
+				  << file_name << std::endl;
+		return;
+	}
+	std::cout << "Image saved to " << file_name << std::endl;
+}
+
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
 		std::cerr << "Usage: " << argv[0] << " <image_path>" << std::endl;
@@ -19,9 +39,11 @@ int main(int argc, char* argv[]) {
 	const char* input_image_path = argv[1];
 
 	// Load the image using stb_image
-	int width, height, channels;
-	unsigned char* original_image = stbi_load(
-		input_image_path, &width, &height, &channels, 3);  // Force load as RGB
+	int width;
+	int height;
+	int channels;
+	unsigned char* original_image =
+		stbi_load(input_image_path, &width, &height, &channels, 1);
 	if (!original_image) {
 		std::cerr << "Error: Could not load image " << input_image_path
 				  << std::endl;
@@ -32,6 +54,18 @@ int main(int argc, char* argv[]) {
 	std::cout << "Image loaded: " << width << "x" << height << " with "
 			  << channels << " channels." << std::endl;
 
+	// Create a Eigen copy of the original matrix
+	Eigen::MatrixXd original_matrix(height, width);
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			const int index = (i * width + j) * channels;
+			original_matrix(i, j) = static_cast<double>(original_image[index]);
+		}
+	}
+
+	// we do not need the original image data
+	stbi_image_free(original_image);
+
 	// Task 2: add a noise in the range [-50;50] to each pixel
 	Eigen::MatrixXd noisy(height, width);
 
@@ -41,54 +75,13 @@ int main(int argc, char* argv[]) {
 
 	for (int i = 0; i < height; i++) {
 		for (int j = 0; j < width; j++) {
-			const int index = (i * width + j) * 3;	// 3 channels (RGB)
-			const double original = static_cast<double>(original_image[index]);
-			noisy(i, j) = static_cast<unsigned char>(
-				std::clamp(original + dist(rnd), 0.0, 255.0));
+			const double original = original_matrix(i, j);
+			const double x = std::clamp(original + dist(rnd), 0.0, 255.0);
+			noisy(i, j) = x;
 		}
 	}
 
-	const std::string noisy_image_path = "noisy.png";
-	if (stbi_write_png(noisy_image_path.c_str(), width, height, 1, noisy.data(),
-					   width) == 0) {
-		std::cerr << "Error: Could not save noisy image" << std::endl;
-
-		return 1;
-	}
-	std::cout << "Noisy image saved to " << noisy_image_path << std::endl;
-
-	// Prepare Eigen matrices for each RGB channel
-	Eigen::MatrixXd red(height, width);
-	Eigen::MatrixXd green(height, width);
-	Eigen::MatrixXd blue(height, width);
-
-	// Fill the matrices with image data
-	for (int i = 0; i < height; ++i) {
-		for (int j = 0; j < width; ++j) {
-			const int index = (i * width + j) * 3;	// 3 channels (RGB)
-			red(i, j) = static_cast<double>(original_image[index]) / 255.0;
-			green(i, j) =
-				static_cast<double>(original_image[index + 1]) / 255.0;
-			blue(i, j) = static_cast<double>(original_image[index + 2]) / 255.0;
-		}
-	}
-
-	// Free memory!!!
-	stbi_image_free(original_image);
-
-	Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic,
-				  Eigen::RowMajor>
-		grayscale_image(height, width);
-
-	// Save the grayscale image using stb_image_write
-	const std::string output_image_path = "output_grayscale.png";
-	if (stbi_write_png(output_image_path.c_str(), width, height, 1,
-					   grayscale_image.data(), width) == 0) {
-		std::cerr << "Error: Could not save grayscale image" << std::endl;
-
-		return 1;
-	}
-	std::cout << "Grayscale image saved to " << output_image_path << std::endl;
+	save_image(noisy, "noisy.png");
 
 	return 0;
 }
